6a2a7dc617cedd8285ff48057c05c0103711ed61
Lee Roy Akbar <leeroyakbar@gmail.com>
2024-03-30 14:58:45 +0700

    improve: welcoming

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
   return (
-    <div className="p-4">
-      <h1>Home</h1>
-      <p>Welcome to your dashboard!</p>
-      <Link to="/login">Login</Link><br />
-      <Link to="/register">Signup</Link>
+    <div className="p-4 d-flex flex-column justify-content-center align-items-center">
+      <h1>Nueron Notes</h1>
+      <p>Welcome to Neuron Notes!</p>
+      <h5>Where you record all your activities, memories, and other things. Happy notes!</h5>
+      <Link to="/login" className="btn btn-primary py-4 d-flex align-items-center btn-icon btn-sm rounded w-50"><b>Log in</b></Link><br />
+      <Link to="/register" className="btn btn-info py-4 d-flex align-items-center btn-icon btn-sm rounded w-50"><b>Sign up</b></Link><br />
     </div>
   )
 }
